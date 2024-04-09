630b16a568b61c0f164c0079edf0721c84c01ae6
rumiyevskiy <rumiyevskiy@gmail.com>
2024-04-09 18:38:44 +0300

    changed swiper direction add text corrected css for text

--- js/script.js
+++ js/script.js
@@ -22,3 +22,29 @@ const swiper = new Swiper('.slider-main-block', {
       delay: 6000,
     },
   });
+
+  const swiper2 = new Swiper('.slider-main-block2', {
+    // Optional parameters
+    loop: true,
+  
+    // // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-main-block__arrow.swiper-button-next',
+      prevEl: '.slider-main-block__arrow.swiper-button-prev',
+    },
+  
+    // // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+
+    autoplay: {
+      delay: 6000,
+      reverseDirection: true,
+    },
+  });
